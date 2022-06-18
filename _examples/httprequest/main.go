94ceb2000282582539e7f83220be2cbda61d2f97
wuriyanto48 <wuriyanto48@yahoo.co.id>
2022-06-18 14:11:18 +0700

    example: add httprequest example

---
+++ _examples/httprequest/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/telkomdev/gaw"
+)
+
+func httpGet(url string) (*http.Response, error) {
+	transport := &http.Transport{
+		Dial: (&net.Dialer{
+			Timeout: 5 * time.Second,
+		}).Dial,
+		TLSHandshakeTimeout: 5 * time.Second,
+		IdleConnTimeout:     10 * time.Second,
+	}
+
+	httpClient := &http.Client{
+		//Timeout:   time.Second * 10,
+		Transport: transport,
+	}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+func main() {
+	url := "https://www.w3schools.com/js/default.asp"
+
+	async := gaw.Async[*http.Response](context.Background(), func() (*http.Response, error) {
+
+		resp, err := httpGet(url)
+
+		return resp, err
+	})
+
+	// do other work while waiting async to finish
+	fmt.Println("do other work")
+
+	// call Await
+	<-async.Await()
+
+	fmt.Println("work done")
+
+	// check if its error
+	if async.IsErr() {
+		fmt.Printf("async error: %v\n", async.Err())
+	} else {
+		resp := async.Get()
+		fmt.Println(resp)
+	}
+}
